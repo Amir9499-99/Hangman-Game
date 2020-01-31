d74820a45047af0b09c169c1d5a15ea978b5771c
jim-clark <email@jim-clark.com>
2020-01-31 13:30:37 -0800

    Add and initialize state

--- js/main.js
+++ js/main.js
@@ -1,9 +1,36 @@
 /*----- constants -----*/
+const WORDS = [
+  'DEVELOPER', 'ENGINEER', 'NODE', 'JAVASCRIPT',
+  'CODING', 'HTML', 'BACK END', 'GUI', 'BOOLEAN',
+  'REACT', 'FUNCTION', 'COMPUTER SCIENCE',
+  'SEPARATION OF CONCERNS'
+];
 
 /*----- app's state (variables) -----*/
+let secretWord;
+let guessWord;
+let usedLetters;
+let wrongLetters;
 
 /*----- cached element references -----*/
 
 /*----- event listeners -----*/
 
 /*----- functions -----*/
+init();
+
+function render() {
+  
+}
+
+function init() {
+  let rndIdx = Math.floor(Math.random() * WORDS.length);
+  secretWord = WORDS[rndIdx];
+  guessWord = '';
+  for (let char of secretWord) {
+    guessWord += char === ' ' ? ' ' : '_';
+  }
+  usedLetters = [];
+  wrongLetters = [];
+  render();
+}
